8bae54f610c61f082b1772b2c8cbfbfc8ed3c073
Manoj Sai Bellamkonda <manojsai.bellamkonda@bounteous.com>
2022-11-03 16:18:30 +0530

    configured reactotron for redux, redux-saga

--- config/ReactotronConfig.ts
+++ config/ReactotronConfig.ts
@@ -1,8 +1,15 @@
-import Reactotron from 'reactotron-react-native'
+import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { reactotronRedux } from 'reactotron-redux'
+import sagaPlugin from 'reactotron-redux-saga';
 
-Reactotron
+const reactotron = Reactotron
   .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure() // controls connection & communication settings
+  .use(reactotronRedux())//for connecting to redux.
+  .use(sagaPlugin())//for connecting to redux saga
   .useReactNative() // add all built-in react native plugins
   .connect() // let's connect!
+
+
+export default reactotron;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,8 +3,16 @@ import pokemonSlice from "./pokemonSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "../saga/pokemonSaga";
 import logger from "redux-logger";
+import Reactotron from '../../config/ReactotronConfig';
 
-let SagaMiddleware = createSagaMiddleware();
+let SagaMiddleware;
+if(__DEV__){
+    let sagaMonitor = Reactotron.createSagaMonitor();
+    SagaMiddleware = createSagaMiddleware({sagaMonitor});
+}else{
+    SagaMiddleware = createSagaMiddleware();
+}
+
 
 
 const store = configureStore({
@@ -12,6 +20,7 @@ const store = configureStore({
         pokemon : pokemonSlice,
     },
     middleware : [SagaMiddleware,logger],
+    enhancers : [Reactotron.createEnhancer()]
 });
 
 SagaMiddleware.run(rootSaga);
